00534352daf0d1cb970a6cb8ab87d94411533437
ILYA <106765406+D15ND@users.noreply.github.com>
2025-12-09 00:27:43 +0300

    feat: add solution

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -152,14 +152,15 @@ function sellTickets(/* queue */) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(width, height) {
-  const obj = {};
-  obj.width = width;
-  obj.height = height;
-  obj.getArea = function () {
-    return this.width * this.height;
-  };
-  return obj;
+function Rectangle(/* width, height */) {
+  // const obj = {};
+  // obj.width = width;
+  // obj.height = height;
+  // obj.getArea = function () {
+  //   return this.width * this.height;
+  // };
+  // return obj;
+  throw new Error('Not implemented');
 }
 
 /**
